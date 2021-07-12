df85673b5a70dfff6755c001e9704bd5cbe8b752
Dalia-H <87212393+Dalia-H@users.noreply.github.com>
2021-07-11 21:59:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ function compute()
     var principal = document.getElementById("principal").value;
     if(principal<1){
         alert("Enter a positive number");
-        focus(principal);
+        document.getElementByType("principal").focus();
     }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
+    
+    document.getElementById("result").innerHTML = "If you deposit" + principal + " ,\<br\> at an interest rate of " + rate + "%\<br\>You will recieve an amount of " + amount + " , \<br\>in the year " + year + "\<br\>"
 }
-document.getElementById("result").innerHTML = "If you deposit" + principal + " ,\<br\> at an interest rate of " + rate + "%\<br\>You will recieve an amount of " + amount + " , \<br\>in the year " + year + "\<br\>"
+
 
 function updateRate()
 {
