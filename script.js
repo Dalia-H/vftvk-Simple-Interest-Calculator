60844dad603fcf9b5b900fb52fd7965024e88488
Dalia-H <87212393+Dalia-H@users.noreply.github.com>
2021-07-11 22:49:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,31 +2,30 @@ function validateNum()
 {
     var principal = document.getElementById("principal").value;
     var moreThanZero = parseInt(principal) > 0;
-    if(!moreThanZero)
-    {
+    if(!moreThanZero){
         alert("Enter a positive number");
         document.getElementById("principal").focus();
     }
 }
-
-function compute()
-{
-    var principal = document.getElementById("principal").value;
-   
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate / 100;
-    var year = new Date().getFullYear()+parseInt(years);
-
-    document.getElementById("result").innerHTML = "If you deposit" + principal + " ,\<br\> at an interest rate of " + rate + "%\<br\>You will recieve an amount of " + amount + " , \<br\>in the year " + year + "\<br\>";
-}
-
 
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
+
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+   
+    var rate = parseInt(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML = "If you deposit" + principal + " ,\<br\> at an interest rate of " + rate + "%\<br\>You will recieve an amount of " + amount + " , \<br\>in the year " + year + "\<br\>";
+}
+
 
 
 
